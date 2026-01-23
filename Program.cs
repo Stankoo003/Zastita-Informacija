42dab2a4ba94e606d8119e0bb0d56ac4506dc901
Stankoo003 <aleksastankovicc.2003@gmail.com>
2026-01-23 10:03:48 +0100

    Initial commit 2

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Net.Sockets;
+using System.IO;
+
+class Program
+{
+    static void Main()
+    {
+        Console.WriteLine("Ovo radi na Mac-u!");
+        
+        // TCP test
+        TcpListener listener = new TcpListener(System.Net.IPAddress.Loopback, 5000);
+        Console.WriteLine("TCP je dostupan!");
+        
+        // File operations
+        File.WriteAllText("test.txt", "Hello Mac!");
+        Console.WriteLine("File I/O je dostupan!");
+    }
+}
